7a2b0cccc30122fbc5336665cef9ccf0206eab3c
Oscar Elmqvist Sandvik <oscar.elmqvistsandvik@gmail.com>
2016-01-18 14:45:24 +0100

    Finished the skeleton by adding abstract function evaluate in newly created Element class and also made a Lattice class

--- accelerator.py
+++ accelerator.py
@@ -8,13 +8,83 @@ from sympy import *
 import math
 import random
 import matplotlib.pyplot as plt
+import abc
 
-class LinearElement:
-    def __init__(self, name):
+class Lattice:
+    def __init__(self,name):
         self.name = name
+        self.lattice = list()
+
+    def appendElement(self, element):
+        self.lattice.append(element)
+
+    def evaluate(self, multipart,envelope):
+        for elem in self.lattice:
+            print "ett varv till"
+            multipart,envelope = elem.evaluate(multipart,envelope)
+
+        return multipart,envelope
+
+class Element:
+    def __init__(self,name):
+        self.name = name
+
+    # abstract evaluate here so that the lattice containing all kinds of elements can be evaluated from the same function
+    @abc.abstractmethod
+    def evaluate(self,multipart,envelope):
+        print "hej fran element"
+        """Calculate the new particle data as the old data goes through"""
+        return
+
+class LinearElement(Element):
+    def __init__(self, name):
+        Element.__init__(self, name)
 
     def printInfo(self):
         return self.name
+
+class Drift(LinearElement):
+    def __init__(self, name, L):
+        LinearElement.__init__(self, name)
+        self.L = L
+        self.M = self.createMatrixM(L) # M should be a 6x6 matrix
+        self.T = self.createMatrixT(L) # M should be a 9x9 matrix
+
+    def createMatrixM(self,L):
+        return np.array([
+                [1,L,0,0,0,0],
+                [0,1,0,0,0,0],
+                [0,0,1,L,0,0],
+                [0,0,0,1,0,0],
+                [0,0,0,0,1,0],
+                [0,0,0,0,0,1]
+                ])
+    def createMatrixT(self, L):
+        return 0
+
+    def evaluate(self,multipart,envelope):
+        print "hej fran drift"
+        newmultipart, newenvelope = self.evaluateSC(multipart,envelope) # evaluate the SC # not needed since it is linear
+        newmultipart, newenvelope = self.evaluateM(newmultipart,newenvelope) # use the new data for "normal" evaluation
+        return newmultipart, newenvelope
+
+    # Evaluate for Space Charges
+    def evaluateSC(self,multipart,envelope):
+        # disunite matrices
+        n = 5
+        Msp, Tsp = self.disunite(self.M,self.T,n)
+        # evaluate the SC
+
+        return 0, 0
+
+    def disunite(self,M,T,n):
+        L_n = self.L/n
+        return 0, 0
+
+
+    def evaluateM(self,multipart,envelope):
+        # something M*multipart
+        return 0, 0
 
 class Quad(LinearElement):
     def __init__(self, name, K, L):
@@ -61,15 +131,17 @@ class Quad(LinearElement):
     def printInfo(self):
         return self.name + "\n" + str(self.M)
 
-    def evaluatue(self,multipart,envelope):
-        newmultipart, newenvelope = evaluateSC(multipart,envelope) # evaluate the SC
-        newmultipart, newenvelope = evaluateM(newmultipart,newenvelope) # use the new data for "normal" evaluation
+    def evaluate(self,multipart,envelope):
+        print "hej fran quad"
+        newmultipart, newenvelope = self.evaluateSC(multipart,envelope) # evaluate the SC # not needed since it is linear
+        newmultipart, newenvelope = self.evaluateM(newmultipart,newenvelope) # use the new data for "normal" evaluation
         return newmultipart, newenvelope
 
     # Evaluate for Space Charges
     def evaluateSC(self,multipart,envelope):
         # disunite matrices
-        Msp, Tsp = disunite(M,T,n)
+        n = 5
+        Msp, Tsp = self.disunite(self.M,self.T,n)
         # evaluate the SC
 
         return 0, 0
@@ -77,6 +149,7 @@ class Quad(LinearElement):
     def disunite(self,M,T,n):
         L_n = self.L/n
         return 0, 0
+
 
     def evaluateM(self,multipart,envelope):
         # something M*multipart
@@ -112,7 +185,7 @@ class Quad(LinearElement):
 
 
 
-class NonLinearElement:
+class NonLinearElement(Element):
     def __init__(self, name, ham, kval, lval, order):
         super(name, self).__init__()
         self.name = name
@@ -145,3 +218,60 @@ class NonLinearElement:
         ypout = self.ypf(mulxin, mulxpin, mulyin, mulypin)
 
         return (xout,xpout,yout,ypout)
+
+# just a copy of Quad so far. TODO: use the scraps from the lie code!
+class Sextu(NonLinearElement):
+    def __init__(self, name, K, L, M):
+        NonLinearElement.__init__(self, name)
+        #self.name = name
+        self.K = K
+        self.L = L
+        self.M = self.createMatrixM(K, L) # M should be a 6x6 matrix
+        self.T = self.createMatrixT(K, L) # M should be a 9x9 matrix
+
+    """ taken from lie code where K = sqrt(k)"""
+
+
+    def printInfo(self):
+        return self.name + "\n" + str(0)
+
+    def evaluate(self,multipart,envelope):
+        newmultipart, newenvelope = evaluateSC(multipart,envelope) # evaluate the SC
+        newmultipart, newenvelope = evaluateM(newmultipart,newenvelope) # use the new data for "normal" evaluation
+        return newmultipart, newenvelope
+
+    # Evaluate for Space Charges
+    def evaluateSC(self,multipart,envelope):
+        # disunite matrices
+        Msp, Tsp = disunite(M,T,n)
+        # evaluate the SC
+
+        return 0, 0
+
+    def disunite(self,M,T,n):
+        L_n = self.L/n
+        return 0, 0
+
+    def evaluateM(self,multipart,envelope):
+        # something M*multipart
+        return 0, 0
+
+# just a copy of Quad so far. TODO: use the scraps from the lie code!
+class Multi(NonLinearElement):
+    def __init__(self, name, K, L, M):
+        NonLinearElement.__init__(self, name)
+        #self.name = name
+        self.K = K
+        self.L = L
+        self.M = self.createMatrixM(K, L) # M should be a 6x6 matrix
+        self.T = self.createMatrixT(K, L) # M should be a 9x9 matrix
+
+# just a copy of Quad so far. TODO: use the scraps from the lie code!
+class Cavity(NonLinearElement):
+    def __init__(self, name, K, L, M):
+        NonLinearElement.__init__(self, name)
+        #self.name = name
+        self.K = K
+        self.L = L
+        self.M = self.createMatrixM(K, L) # M should be a 6x6 matrix
+        self.T = self.createMatrixT(K, L) # M should be a 9x9 matrix
--- testfacility.py
+++ testfacility.py
@@ -1,4 +1,4 @@
-from accelerator import LinearElement, Quad
+from accelerator import Lattice, Element, LinearElement, Quad, Drift
 
 sigma = 0.001 # the standard deviation that the user will enter
 #epsilon = sqrt(sigmax**2*sigmaxp**2-sigmaxxp**2)
@@ -6,8 +6,15 @@ sigma = 0.001 # the standard deviation that the user will enter
 quad = Quad('quad', 0.01, 1)
 print quad.printInfo()
 
-
+#quad.evaluate(0,0)
 
 
 ## Non-linear
 order = 5 # user sets this
+
+## Lattice construction
+drift = Drift('drift', 1)
+lattice = Lattice('lattice')
+lattice.appendElement(drift)
+lattice.appendElement(quad)
+lattice.evaluate(0,0)
