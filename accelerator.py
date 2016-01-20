7983a9c73bd594106272811357481e5ff600eb94
Oscar Elmqvist Sandvik <oscar.elmqvistsandvik@gmail.com>
2016-01-20 16:51:04 +0100

    Added space charge for drift but am considering making space charge its own class so also started on that. The idea is that each element object will have its own space charge object so that I won't have to implement space charge many times in every element.

--- accelerator.py
+++ accelerator.py
@@ -51,6 +51,7 @@ class LinearElement(Element):
     def printInfo(self):
         return self.name
 
+### DRIFT !!!!
 class Drift(LinearElement):
     def __init__(self, name, L):
         LinearElement.__init__(self, name)
@@ -58,10 +59,13 @@ class Drift(LinearElement):
         self.M = self.createMatrixM(L) # M should be a 6x6 matrix
         self.T = self.createMatrixT(L) # M should be a 9x9 matrix
 
-        # disunite the matrices
+        # disunite the matrices, CLEAN UP THIS CODE!!!!!! (should disunite be removed or kept?)
         self.n = 5
         self.Lsp = self.L/self.n
         self.Msp, self.Tsp = self.disunite(self.M,self.T,self.n)
+
+        # and the space charge matrices for the disunited matrices (SC always implies a split matrix)
+        self.Msc, self.Tsc = self.spaceChargeMatrix()
 
     def createMatrixM(self,L):
         return np.array([
@@ -75,6 +79,47 @@ class Drift(LinearElement):
     def createMatrixT(self, L):
         return 0
 
+    def disunite(self,M,T,n):
+        Msp = self.createMatrixM(self.Lsp)
+        return Msp, 0
+
+    #Maybe the space charge should be its own class????????
+    def spaceChargeMatrix(self):
+        deltas = self.Lsp
+
+        # beam data is needed as input to calculate the following variables...
+        beta = 0.9 # beam data needed
+        gamma = 1/sqrt(1-beta**2)
+        m = 1.0 # should be proton mass but would rather have mass and q as input so that other particles can be used
+        q = 1.0 # -II-
+        c = 299792458.0 # in metric (metric for everything perhaps?) 
+
+        # <.> is called "norm_of_."
+        # <x^2> (norm_of_xx), <xx'> (norm_of_xx') and <x'^2> (norm_of_xpxp) come from (119) in ref 1.
+        norm_of_xx = 1.0
+        # <x> (norm_of_x) come from !!!
+        norm_of_x = 0.9
+        # <xE_x> (norm_of_xE_x) come from !!!
+        norm_of_xE_x = 1.0
+
+        f_scx = gamma**3*beta**2*m*c**2/q*(norm_of_xx - norm_of_x**2)/norm_of_xE_x # shoud really be eqn (152) in ref 1.
+        f_scy = 1.0 # shoud really be eqn (152) (with x->y) in ref 1.
+        f_scz = 1.0 # shoud really be eqn (152) (with x->z) in ref 1.
+        xbar = 1.0
+        ybar = 1.0
+        zbar = 1.0
+
+        Msc = np.array([
+                [1,0,0,0,0,0,0],
+                [deltas/f_scx,1,0,0,0,0,-xbar*deltas/f_scx],
+                [0,0,1,0,0,0,0],
+                [0,0,deltas/f_scy,1,0,0,-ybar*deltas/f_scy],
+                [0,0,0,0,1,0,0],
+                [0,0,0,0,deltas/f_scz,1,-zbar*deltas/f_scz],
+                [0,0,0,0,0,0,1]
+            ])
+        return Msc, 0
+
     def evaluate(self,multipart,envelope):
         # some for loop that goes through all of the disunited parts
         #newmultipart, newenvelope = multipart, envelope
@@ -83,34 +128,31 @@ class Drift(LinearElement):
         #print "hej fran drift"
         #print "evaluate multipart: " + str(multipart)
 
+            # The should be a check if Msc and Tsc need to be updated if the beam properties have changed a lot!!!!!!!!
+            #if beamChanged(beam):
+                #self.Msc, self.Tsc = spaceChargeMatrix()
+
             multipart, envelope = self.evaluateSC(multipart,envelope) # evaluate the SC # not needed since it is linear
             multipart, envelope = self.evaluateM(multipart,envelope) # use the new data for "normal" evaluation
         return multipart, envelope
 
-    # Evaluate for Space Charges
+    # Evaluate for Space Charges, se ref A.
     def evaluateSC(self,multipart,envelope):
         # evaluate the SC
         # for n
         #     newdata = Msp*olddata
         #     olddata = newdata
 
-        SC = 0
+        SC = 1
         if not SC:
             return multipart, envelope # just returns the same values as of now!
         else:
-            return 0,0
-
-    def disunite(self,M,T,n):
-        Msp = np.array([
-                [1,self.Lsp,0,0,0,0],
-                [0,1,0,0,0,0],
-                [0,0,1,self.Lsp,0,0],
-                [0,0,0,1,0,0],
-                [0,0,0,0,1,self.Lsp],
-                [0,0,0,0,0,1]
-                ])
-        return Msp, 0
-
+            for j in range(0,len(np.atleast_1d(multipart))):
+                extendedphasespace = np.vstack((multipart[j][0][0:6], 1)) # adds the dispersion 1 term
+                extendedphasespace = np.dot(self.Msc, extendedphasespace) # here calculations are made
+                reducedphasespace = extendedphasespace[0:6] # throws away the dispersion 1 term
+                multipart[j] = np.array([reducedphasespace, multipart[j][1]]) # s remains the same because the particles don't go anywhere. They "go" in evaluateM()
+            return multipart,envelope
 
     def evaluateM(self,multipart,envelope):
         # should just go through a disunited part
@@ -118,6 +160,8 @@ class Drift(LinearElement):
             multipart[j] = np.array([np.dot(self.Msp, multipart[j][0][0:6]), multipart[j][1] + self.Lsp])
         return multipart, 0
 
+
+### QUAD
 class Quad(LinearElement):
     def __init__(self, name, K, L):
         LinearElement.__init__(self, name)
@@ -165,6 +209,10 @@ class Quad(LinearElement):
     def createMatrixT(self, K, L):
         return 0
 
+    def disunite(self,M,T,n):
+        Msp = self.createMatrixM(self.K, self.Lsp)
+        return Msp, 0
+
     def printInfo(self):
         return self.name + "\n" + str(self.M)
 
@@ -186,11 +234,6 @@ class Quad(LinearElement):
         else:
             return 0,0
 
-    def disunite(self,M,T,n):
-        Msp = self.createMatrixM(self.K, self.Lsp)
-        return Msp, 0
-
-
     def evaluateM(self,multipart,envelope):
         # should just go through a disunited part
         # each loop iteration is for a new particle
@@ -198,10 +241,79 @@ class Quad(LinearElement):
             multipart[j] = np.array([np.dot(self.Msp, multipart[j][0][0:6]), multipart[j][1] + self.Lsp])
         return multipart, 0
 
+### SPACE CHARGE!!!!!
+class SpaceCharge(LinearElement):
+    def __init__(self, name, deltas,beam):
+        LinearElement.__init__(self, name)
+        self.deltas = deltas
+        self.beam = beam
 
+        self.Msc = spaceChargeMatrix()
 
+    def updateBeam(self, beam):
+        self.beam = beam
+        return
 
+    def beamChanged(self, newbeam):
+        threshold = 0.1
+        if diffBtwBeams(self.beam,newbeam) > threshold:
+            return 1
+        else:
+            return 0
 
+    def diffBtwBeams(self, beam1,beam2):
+        diff = 0
+#        for bla
+#            diff += diff_each_variable
+        return diff
+
+    def spaceChargeMatrix(self, beam):
+        deltas = self.Lsp
+
+        # beam data is needed as input to calculate the following variables...
+        beta = 0.9 # beam data needed
+        gamma = 1/sqrt(1-beta**2)
+        m = 1.0 # should be proton mass but would rather have mass and q as input so that other particles can be used
+        q = 1.0 # -II-
+        c = 299792458.0 # in metric (metric for everything perhaps?) 
+
+        # <.> is called "norm_of_."
+        # <x^2> (norm_of_xx), <xx'> (norm_of_xx') and <x'^2> (norm_of_xpxp) come from (119) in ref 1.
+        norm_of_xx = 1.0
+        # <x> (norm_of_x) come from !!!
+        norm_of_x = 0.9
+        # <xE_x> (norm_of_xE_x) come from !!!
+        norm_of_xE_x = 1.0
+
+        f_scx = gamma**3*beta**2*m*c**2/q*(norm_of_xx - norm_of_x**2)/norm_of_xE_x # shoud really be eqn (152) in ref 1.
+        f_scy = 1.0 # shoud really be eqn (152) (with x->y) in ref 1.
+        f_scz = 1.0 # shoud really be eqn (152) (with x->z) in ref 1.
+        xbar = 1.0
+        ybar = 1.0
+        zbar = 1.0
+
+        Msc = np.array([
+                [1,0,0,0,0,0,0],
+                [deltas/f_scx,1,0,0,0,0,-xbar*deltas/f_scx],
+                [0,0,1,0,0,0,0],
+                [0,0,deltas/f_scy,1,0,0,-ybar*deltas/f_scy],
+                [0,0,0,0,1,0,0],
+                [0,0,0,0,deltas/f_scz,1,-zbar*deltas/f_scz],
+                [0,0,0,0,0,0,1]
+            ])
+        return Msc
+
+    def evaluateSC(self,multipart,envelope):
+        for j in range(0,len(np.atleast_1d(multipart))):
+            # The should be a check if Msc and Tsc need to be updated if the beam properties have changed a lot!!!!!!!!
+            #if beamChanged(envelope):
+                #self.Msc, self.Tsc = spaceChargeMatrix(envlope)
+
+            extendedphasespace = np.vstack((multipart[j][0][0:6], 1)) # adds the dispersion 1 term
+            extendedphasespace = np.dot(self.Msc, extendedphasespace) # here calculations are made
+            reducedphasespace = extendedphasespace[0:6] # throws away the dispersion 1 term
+            multipart[j] = np.array([reducedphasespace, multipart[j][1]]) # s remains the same because the particles don't go anywhere. They "go" in evaluateM()
+        return multipart,envelope
 
 
 
@@ -326,3 +438,7 @@ class Cavity(NonLinearElement):
         self.L = L
         self.M = self.createMatrixM(K, L) # M should be a 6x6 matrix
         self.T = self.createMatrixT(K, L) # M should be a 9x9 matrix
+
+# references
+# 1. simulatingbeamswithellipsoidalsymmetry-secondedition
+# A. 7.2. Space Charge Impulses in simulatingbeamswithellipsoidalsymmetry-secondedition
