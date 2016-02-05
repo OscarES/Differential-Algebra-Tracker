328e7a07932584bc939839e892881a4ce0913f9b
Oscar Elmqvist Sandvik <oscar.elmqvistsandvik@gmail.com>
2016-02-05 12:40:33 +0100

    Renamed variables and translated all comments into english in diff.py . Used diff.py to compare the results from lie and DAT codes, the standard deviation of the error between them is just the power of e-13 ! Which means that the results match perfectly! :)

--- diff.py
+++ diff.py
@@ -1,18 +1,18 @@
 import numpy as np
 
-##### Diff mellan mina o antons resultat, datan ska va sparad som x column xp column
-antonsdata = raw_input('Enter anton\'s datafile name:')
-if len(antonsdata) < 1 : antonsdata = "100outanton.txt" # if no filename given, this file will be used
-oscarsdata = raw_input('Enter oscar\'s datafile name:')
-if len(oscarsdata) < 1 : oscarsdata = "out100oscarh.txt" # if no filename given, this file will be used
+##### Diff between particle data, data should be stored as x column xp column y column yp
+firstdata = raw_input('Enter first datafile name:')
+if len(firstdata) < 1 : firstdata = "data/comparison/" + "outjustFODSOpartfromlie.txt" # if no filename given, this file will be used
+seconddata = raw_input('Enter second datafile name:')
+if len(seconddata) < 1 : seconddata = "data/comparison/" + "outpartFODSOfromDAT.txt" # if no filename given, this file will be used
 
-antonx, antonxp, antony, antonyp = np.loadtxt(antonsdata,unpack = True)
-oscarx, oscarxp, oscary, oscaryp = np.loadtxt(oscarsdata,unpack = True)
+firstx, firstxp, firsty, firstyp = np.loadtxt(firstdata,unpack = True)
+secondx, secondxp, secondy, secondyp = np.loadtxt(seconddata,unpack = True)
 
-diffx = antonx - oscarx
-diffxp = antonxp - oscarxp
-diffy = antony - oscary
-diffyp = antonyp - oscaryp
+diffx = firstx - secondx
+diffxp = firstxp - secondxp
+diffy = firsty - secondy
+diffyp = firstyp - secondyp
 
 stdx = np.std(diffx)
 stdxp = np.std(diffxp)
@@ -25,11 +25,11 @@ print 'stdy:',stdy
 print 'stdyp:',stdyp
 
 # std for xin,xpin,yin,ypin
-#print 'Initial beam std (when antonsdata is the init while and not results...)'
-#print 'stdx:',np.std(antonx)
-#print 'stdxp:',np.std(antonxp)
-#print 'stdy:',np.std(antony)
-#print 'stdyp:',np.std(antonyp)
+#print 'Initial beam std (when firstsdata is the init while and not results...)'
+#print 'stdx:',np.std(firstx)
+#print 'stdxp:',np.std(firstxp)
+#print 'stdy:',np.std(firsty)
+#print 'stdyp:',np.std(firstyp)
 
 ## TODO: 
 #1: make the program work by calling something like: python diff.py out.txt out2.txt
