c323f6c855aef53f650c85e5d4e58618d3f6f3d6
Oscar Elmqvist Sandvik <oscar.elmqvistsandvik@gmail.com>
2016-02-05 09:10:09 +0100

    Added try/except to the load functions

--- IOHandler.py
+++ IOHandler.py
@@ -10,35 +10,55 @@ def saveAll(filename, multipart, twiss, envelope, lattice):
     return 1
 
 def loadAll(filename):
-    multipart = loadMultipart(filename + "multipart" + ".npy")
-    twiss = loadTwiss(filename + "twiss" + ".npy")
-    envelope = loadEnvelope(filename + "envelope" + ".npy")
-    lattice = loadLattice(filename + "lattice" + ".npy")
-    return multipart, twiss, envelope, lattice
+    try:
+        multipart = loadMultipart(filename + "multipart" + ".npy")
+        twiss = loadTwiss(filename + "twiss" + ".npy")
+        envelope = loadEnvelope(filename + "envelope" + ".npy")
+        lattice = loadLattice(filename + "lattice" + ".npy")
+        return multipart, twiss, envelope, lattice
+    except:
+        print 'Bad datafile!'
+        quit()
+        return 0
 
 def saveMultipart(filename, multipart):
     np.save(filename, multipart)
     return 1
 
 def loadMultipart(filename):
-    multipart = np.load(filename)
-    return multipart
+    try:
+        multipart = np.load(filename)
+        return multipart
+    except:
+        print 'Bad datafile!'
+        quit()
+        return 0
 
 def saveTwiss(filename, twiss):
     np.save(filename, twiss)
     return 1
 
 def loadTwiss(filename):
-    twiss = np.load(filename)
-    return twiss
+    try:
+        twiss = np.load(filename)
+        return twiss
+    except:
+        print 'Bad datafile!'
+        quit()
+        return 0
 
 def saveEnvelope(filename, envelope):
     np.save(filename, envelope)
     return 1
 
 def loadEnvelope(filename):
-    envelope = np.load(filename)
-    return envelope
+    try:
+        envelope = np.load(filename)
+        return envelope
+    except:
+        print 'Bad datafile!'
+        quit()
+        return 0
 
 def saveLattice(filename, lattice):
     #np.save(filename, lattice)
@@ -48,12 +68,21 @@ def saveLattice(filename, lattice):
     return 1
 
 def loadLattice(filename):
-    #lattice = np.load(filename)
-    lattice = pickle.load(open(filename, 'rb'))
-    return lattice
+    try:
+        lattice = pickle.load(open(filename, 'rb'))
+        return lattice
+    except:
+        print 'Bad datafile!'
+        quit()
+        return 0
 
 def saveSummer2015Format():
     return 0
 
 def loadSummer2015Format():
-    return 0
+    try:
+        return 0
+    except:
+        print 'Bad datafile!'
+        quit()
+        return 0
