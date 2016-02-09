df79182df1353fe06823a0a463baa47a435ee688
Oscar Elmqvist Sandvik <oscar.elmqvistsandvik@gmail.com>
2016-02-09 10:51:50 +0100

    Added a function to calculate relativistic beta from m_0 and energy assuming large energy

---
+++ relativity.py
@@ -0,0 +1,7 @@
+from __future__ import division # needed for 1/2 = 0.5
+import math
+from scipy import constants
+from sympy import *
+
+def betaFromE(m_0,E):
+    return sqrt(1-m_0**2*constants.c**4/E**2)
