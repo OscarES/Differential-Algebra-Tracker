f028f31a86dc6c1b1a4e02d3129e25d6e162c5c7
Oscar Elmqvist Sandvik <oscar.elmqvistsandvik@gmail.com>
2016-03-18 14:14:56 +0100

    Added a gammaFromBeta function

--- relativity.py
+++ relativity.py
@@ -7,3 +7,6 @@ from sympy import *
 def betaFromE(m_0,E):
     gammaSquared = E**2/m_0**2/constants.c**4+2*E/m_0/constants.c**2+1
     return sqrt(1-1/gammaSquared)
+
+def gammaFromBeta(beta):
+    return 1/sqrt(1-beta**2)
