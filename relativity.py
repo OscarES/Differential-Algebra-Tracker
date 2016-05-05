af82a39bf1929d68559d27d0ea322fac74371c94
Oscar Elmqvist Sandvik <oscar.elmqvistsandvik@gmail.com>
2016-05-05 14:03:15 +0200

    Turned on the old way of betaFromE since the new one would give quad matrices that wasn't the same as the ones produced by TraceWin.

--- relativity.py
+++ relativity.py
@@ -5,10 +5,10 @@ from sympy import *
 
 # assumes a large energy
 def betaFromE(m_0,E):
-    #gammaSquared = E**2/m_0**2/constants.c**4+2*E/m_0/constants.c**2+1
+    gammaSquared = E**2/m_0**2/constants.c**4+2*E/m_0/constants.c**2+1 # This will give the same matrices as TraceWin!!
+    return sqrt(1-1/gammaSquared) # ^^
+    #gammaSquared = (E/m_0/constants.c**2)**2 # wikipedia Energy-momentum_relation, which might be more correct
     #return sqrt(1-1/gammaSquared)
-    gammaSquared = (E/m_0/constants.c**2)**2 # wikipedia Energy-momentum_relation
-    return sqrt(1-1/gammaSquared)
 
 # assumes a large energy and not 100 % sure but I have calcs of this starting from the betaFromE and gammaFromBeta functions
 def EFromBeta(m_0,beta):
